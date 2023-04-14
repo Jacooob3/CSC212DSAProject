d4dfb848743de13956d59be9d2ca119e69d48f07
ivanabreu0 <102328381+ivanabreu0@users.noreply.github.com>
2023-04-14 01:17:46 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,108 @@
-#include "bst.h"
-#include <fstream>
-#include <sstream>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <ostream>
+#include <sstream>
+#include "bst.h"
 
-void print_tree(BST* tree, int mode, std::ofstream& os);
+void makeSchedule(std::string file_name,BST tree) {};
 
-int main(int argc, char*argv[]){
-    std::ifstream ifs(argv[1]);
-    int mode = std::stoi(argv[2]);
-    std::string fname(argv[3]);
+int main() {
 
-    std::ofstream fs(fname);
+    std::string file_name;
+    bool correct_file = false;
+    while (!correct_file) {
+        std::cout << "Enter file name:" << std::endl;
+        std::cin >> file_name;
+
+        std::fstream file(file_name);
+
+        if (file.is_open()) {
+            correct_file = true;
+        }
+        else {
+            std::cout << "File " << file_name << " cannot be found. Try again" << std::endl;
+        }
+    }
 
     BST tree;
 
-    std::string line;
+    makeSchedule(file_name, tree);
 
-    // Get the numbers to be inserted
-    std::getline(ifs, line);
-    std::stringstream ss(line);
+    std::string out_file;
 
-    int num;
-    // Insert the numbers
-    while(ss >> num){
-        tree.insert(num);
-    }
-    print_tree(&tree, mode, fs);
-    fs << "Tree Height: " << tree.getHeight() - 1 << std::endl;
-    fs.close();
-}
+    std::cout << "Enter name of output file (.csv):" << std::endl;
+    std::cin >> out_file;
 
-/*
- * pre = 1
- * in = 2
- * post = 4
- * pre+in = 3
- * pre+post = 5
- * in+post = 6
- * pre+in+post = 7
- */
-void print_tree(BST* tree, int mode, std::ofstream& os){
-    if(mode == 1 || mode == 3 || mode == 5 || mode == 7){
-        tree->preorder(os);
+    bool cont = true;
+    std::string command;
+
+    while (cont) {
+        std::cout << "If you want to print out all students' schedules, enter 'ALL'" << std::endl;
+        //Maybe can add this as an option
+        //std::cout << "If you want to see the amount of students in a course, enter 'COURSE'" << std::endl;
+        std::cout << "If you want to search and print a student's schedule, enter 'SEARCH'" << std::endl;
+        std::cout << "If you want to add a new student and their schedule, enter 'ADD'" << std::endl;
+        std::cout << "If you want to delete a student, enter 'DELETE'" << std::endl;
+        std::cout << "If you want to enroll a student in an additional course, enter 'ENROlL'" << std::endl;
+        std::cout << "If you want to unenroll a student from a course, enter 'UNENROLL'" << std::endl;
+        std::cin >> command;
+
+        //Adding code to allow these commands to run
+        if (command == "ALL") {
+
+        }
+        else if (command == "SEARCH") {
+            int id_num;
+            std::string id_string;
+
+            std::cout << "Enter new student's ID number:" << std::endl;
+            std::cin >> id_string;
+
+        }
+        else if (command == "ADD") {
+            int id_num;
+            std::string id_string;
+
+            std::cout << "Enter new student's ID number:" << std::endl;
+            std::cin >> id_string;
+
+            //can either continue with this and take inputs 1 by 1, or have user input a csv file with new student's details
+
+        }
+        else if (command == "DELETE") {
+            int id_num;
+            std::string id_string;
+
+            std::cout << "Enter student's ID number:" << std::endl;
+            std::cin >> id_string;
+
+            //delete function here
+
+        }
+        else if (command == "ENROLL") {
+            int id_num;
+            std::string id_string;
+
+            std::cout << "Enter student's ID number:" << std::endl;
+            std::cin >> id_string;
+
+        }
+        else if (command == "UNENROLL") {
+            int id_num;
+            std::string id_string;
+
+            std::cout << "Enter new student's ID number:" << std::endl;
+            std::cin >> id_string;
+
+        }
+        else {
+            std::cout << "Wrong command inputted, try again." << std::endl;
+        }
     }
-    if(mode == 2 || mode == 3 || mode == 6 || mode == 7){
-        tree->inorder(os);
-    }
-    if(mode == 4 || mode == 5 || mode == 6 || mode == 7){
-        tree->postorder(os);
-    }
+
+
+
+
+    return 0;
 }
