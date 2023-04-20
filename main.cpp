a2b2081704f0975946f4f00992bf5c60a3b10ad7
Jacob Hyun <98486911+Jacooob3@users.noreply.github.com>
2023-04-20 18:32:24 -0400

    Update main.cpp
    Created a function for the open file try and error
    added uppercase function for the input for commands so any input as long as it is typed correctly can be read

--- main.cpp
+++ main.cpp
@@ -3,40 +3,28 @@
 #include <fstream>
 #include <ostream>
 #include <sstream>
+#include <locale>
+
 #include "bst.h"
 
 void makeSchedule(std::string file_name,BST tree) {};
+std::string uppercase(std::string command);
+std::string openFile();
 
 int main() {
-
-    std::string file_name;
-    bool correct_file = false;
-    while (!correct_file) {
-        std::cout << "Enter file name:" << std::endl;
-        std::cin >> file_name;
-
-        std::fstream file(file_name);
-
-        if (file.is_open()) {
-            correct_file = true;
-        }
-        else {
-            std::cout << "File " << file_name << " cannot be found. Try again" << std::endl;
-        }
-    }
+    //open file
+    std::string file_name = openFile();
 
     BST tree;
 
     makeSchedule(file_name, tree);
 
     std::string out_file;
-
     std::cout << "Enter name of output file (.csv):" << std::endl;
     std::cin >> out_file;
 
     bool cont = true;
     std::string command;
-
     while (cont) {
         std::cout << "If you want to print out all students' schedules, enter 'ALL'" << std::endl;
         //Maybe can add this as an option
@@ -47,7 +35,7 @@ int main() {
         std::cout << "If you want to enroll a student in an additional course, enter 'ENROlL'" << std::endl;
         std::cout << "If you want to unenroll a student from a course, enter 'UNENROLL'" << std::endl;
         std::cin >> command;
-
+        command = uppercase(command);
         //Adding code to allow these commands to run
         if (command == "ALL") {
 
@@ -100,9 +88,30 @@ int main() {
             std::cout << "Wrong command inputted, try again." << std::endl;
         }
     }
-
-
-
-
     return 0;
+}
+std::string openFile(){
+    std::string file_name;
+    bool correct_file = false;
+    while (!correct_file) {
+        std::cout << "Enter file name:" << std::endl;
+        std::cin >> file_name;
+
+        std::fstream file(file_name);
+
+        if (file.is_open()) {
+            return file_name;
+        }
+        else {
+            std::cout << "File " << file_name << " cannot be found. Try again" << std::endl;
+        }
+    }
+}
+std::string uppercase(std::string command){
+    std::locale loc;
+    std::string temp = command;
+    for(int i = 0; i < command.length(); i++){
+        std::toupper(temp[i], loc);
+    }
+    return temp;
 }
