4c182d709377854779b5a5feef240e88a2456cb2
ivanabreu0 <102328381+ivanabreu0@users.noreply.github.com>
2023-04-23 05:36:43 -0400

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -13,3 +13,27 @@ Student::Student(int num, std::string firstname, std::string lastname, std::vect
 int Student::get_id(){
     return this->id;
 }
+
+std::vector<std::string> Student::get_classes() {
+    return this->classes;
+}
+
+std::pair<std::string, std::string> Student::get_name() {
+    std::pair<std::string, std::string> temp(this->first_name,this->last_name);
+    return temp;
+}
+
+void Student::add_classes(std::string class_name) {
+    this->classes.push_back(class_name);
+}
+
+void Student::remove_classes(std::string class_name) {
+    std::vector<std::string> temp;
+    for (int i = 0; i < this->classes.size(); i++) {
+        if (this->classes[i] == class_name) {
+            continue;
+        }
+        temp.push_back(this->classes[i]);
+    }
+    this->classes = temp;
+}
